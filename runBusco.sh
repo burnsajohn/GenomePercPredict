cbe94ef2b645b327639fe61757c68df9b61746c3
burnsajohn <burnsajohn@gmail.com>
2021-09-29 12:56:31 -0400

    Add files via upload

--- runBusco.sh
+++ runBusco.sh
@@ -5,6 +5,7 @@
 ###usage: runBuscoSubs.sh -d [subsample directory] -o [output directory] -e [busco directory]
 ###removes busco directory after each run, but saves short summary file
 ###can be parallelized; can likely be optimized to only run BUSCO once on whole protein file and search for hits using shuffled subsample fasta headers. Fairly slow and inefficient when run linearly
+###writes table at end: BUSCOtable.csv that is used for compiling results
 
 
 while getopts d:o:e: flag
@@ -20,8 +21,20 @@ done
 for f in $directory/*.fa* ##for all subsample fasta files 
 do
 echo "working on file $f"
-outname="$(basename -- $f)"
-busco -m protein -i $f -o busco$outname -l $eukdb
-mv busco$outname/short_summary* $outdir
-rm -r busco$outname/
+outname="$(basename -- $f)" ###remove directory info
+outname2=$(echo "$outname" | cut -f 1 -d '.') ###remove file extension (.fasta or .fa, etc.)
+busco -m protein -i $f -o busco$outname2 -l $eukdb
+mv busco$outname2/short_summary* $outdir
+rm -r busco$outname2/
 done
+
+echo "finished BUSCO, collating table"
+
+for f in $outdir/*.txt ##making a data table  
+do
+var=$(grep -v "^#" $f | grep "Missing" | sed 's/Missing BUSCOs (M)//' | sed -e 's/^[[:space:]]*//') ##collect number missing from busco short summary 
+outname="$(basename -- $f)"
+label=$(echo $outname | sed 's/short_summary.specific.eukaryota_odb10.busco//' | sed 's/.txt//' | sed 's/\///' | sed -e 's/\./,/2;s/\./,/2') ##remove unnecessary addons, remove period and replace with a comma, but retain period in fractional percentage number  
+echo "$label,$var" >> $outdir/BUSCOtable.csv
+done
+
